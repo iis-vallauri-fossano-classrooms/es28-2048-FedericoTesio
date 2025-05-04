5bb95f24c093e605956ac02096ba256bd927558c
FedericoTesio <f.tesio.3619@vallauri.edu>
2025-05-04 16:09:33 +0200

    Aggiunto controllo sulla sconfitta e funzione celle
    Aggiunto il controllo sulla sconfitta, cioè quando le caselle sono tutte piene e non si può più fare alcuna mossa e aggiunta la funzione genera cella per generare una nuova cella dopo ogni click sulla freccia

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 const DIM = 4;
 let punti = 0;
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
    const wrapper = document.getElementById("wrapper");
 
-   for(let i = 0; i < DIM; i++){
-      for(let j = 0; j < DIM; j++){
+   for (let i = 0; i < DIM; i++) {
+      for (let j = 0; j < DIM; j++) {
          let div = document.createElement("div");
          div.id = `cella-${i}-${j}`;
          div.classList.add("cella");
@@ -16,7 +16,7 @@ window.addEventListener("load", function(){
    }
 
    let pos1 = [0, 0], pos2 = [0, 0];
-   do{
+   do {
       pos1[0] = generaNumero(0, 4);
       pos1[1] = generaNumero(0, 4);
       pos2[0] = generaNumero(0, 4);
@@ -29,23 +29,23 @@ window.addEventListener("load", function(){
    window.addEventListener("keydown", keyClicked);
 });
 
-function generaNumero(min, max){
-   return Math.floor((max - min) * Math.random()) + min;   
+function generaNumero(min, max) {
+   return Math.floor((max - min) * Math.random()) + min;
 }
 
-function keyClicked(event){
-   switch(event.key){
+function keyClicked(event) {
+   switch (event.key) {
       case "ArrowUp":
-         ArrowUpClicked();
+         arrowUpClicked();
          break;
       case "ArrowDown":
-         ArrowDownClicked();
+         arrowDownClicked();
          break;
       case "ArrowLeft":
-         ArrowLeftClicked();
+         arrowLeftClicked();
          break;
       case "ArrowRight":
-         ArrowRightClicked();
+         arrowRightClicked();
          break;
       default:
          alert("Premi una delle freccie");
@@ -53,128 +53,203 @@ function keyClicked(event){
    }
 }
 
-function ArrowUpClicked(){
-   for(let i = DIM - 1; i > 0; i--){
-      for(let j = DIM - 1; j > -1; j--){
+function arrowUpClicked() {
+   let spostamenti = false;
+   for (let i = DIM - 1; i > 0; i--) {
+      for (let j = DIM - 1; j > -1; j--) {
          let z = DIM - 1;
-         while(z > i){
-            if(document.getElementById(`cella-${z - 1}-${j}`).innerText == ""){
+         while (z > i) {
+            if (document.getElementById(`cella-${z - 1}-${j}`).innerText == "") {
                document.getElementById(`cella-${z - 1}-${j}`).innerText = document.getElementById(`cella-${z}-${j}`).innerText;
                document.getElementById(`cella-${z}-${j}`).innerText = "";
             }
-            else if(document.getElementById(`cella-${z}-${j}`).innerText == document.getElementById(`cella-${z - 1}-${j}`).innerText){
-               document.getElementById(`cella-${z - 1}-${j}`).innerText = parseInt(document.getElementById(`cella-${z}-${j}`).innerText)*2;
+            else if (document.getElementById(`cella-${z}-${j}`).innerText == document.getElementById(`cella-${z - 1}-${j}`).innerText) {
+               document.getElementById(`cella-${z - 1}-${j}`).innerText = parseInt(document.getElementById(`cella-${z}-${j}`).innerText) * 2;
                document.getElementById(`cella-${z}-${j}`).innerText = "";
                punti += parseInt(document.getElementById(`cella-${z - 1}-${j}`).innerText);
                document.getElementsByTagName("span")[0].innerText = punti;
+               spostamenti = true;
             }
             z--;
          }
          z = i;
-         while(z > 0){
-            if(document.getElementById(`cella-${z - 1}-${j}`).innerText == ""){
+         while (z > 0) {
+            if (document.getElementById(`cella-${z - 1}-${j}`).innerText == "") {
                document.getElementById(`cella-${z - 1}-${j}`).innerText = document.getElementById(`cella-${z}-${j}`).innerText;
                document.getElementById(`cella-${z}-${j}`).innerText = "";
             }
-            else if(document.getElementById(`cella-${z}-${j}`).innerText == document.getElementById(`cella-${z - 1}-${j}`).innerText){
-               document.getElementById(`cella-${z - 1}-${j}`).innerText = parseInt(document.getElementById(`cella-${z}-${j}`).innerText)*2;
+            else if (document.getElementById(`cella-${z}-${j}`).innerText == document.getElementById(`cella-${z - 1}-${j}`).innerText) {
+               document.getElementById(`cella-${z - 1}-${j}`).innerText = parseInt(document.getElementById(`cella-${z}-${j}`).innerText) * 2;
                document.getElementById(`cella-${z}-${j}`).innerText = "";
                punti += parseInt(document.getElementById(`cella-${z - 1}-${j}`).innerText);
                document.getElementsByTagName("span")[0].innerText = punti;
+               spostamenti = true;
             }
             z--;
          }
       }
    }
 
-   let pos1 = [0, 0];
-   do{
-      pos1[0] = generaNumero(0, 4);
-      pos1[1] = generaNumero(0, 4);
-   } while (document.getElementById(`cella-${pos1[0]}-${pos1[1]}`).innerText != "");
+   if (!spostamenti) {
+      let lose = checkLose();
+      if (lose) {
+         return;
+      }
+   }
 
-   document.getElementById(`cella-${pos1[0]}-${pos1[1]}`).innerText = "2";
+   generaCella();
 }
 
-function ArrowDownClicked(){
-   for(let i = 0; i < DIM - 1; i++){
-      for(let j = 0; j < DIM; j++){
+function arrowDownClicked() {
+   let spostamenti = false;
+   for (let i = 0; i < DIM - 1; i++) {
+      for (let j = 0; j < DIM; j++) {
          let z = 0;
-         while(z < i){
-            if(document.getElementById(`cella-${z + 1}-${j}`).innerText == ""){
+         while (z < i) {
+            if (document.getElementById(`cella-${z + 1}-${j}`).innerText == "") {
                document.getElementById(`cella-${z + 1}-${j}`).innerText = document.getElementById(`cella-${z}-${j}`).innerText;
                document.getElementById(`cella-${z}-${j}`).innerText = "";
+               spostamenti = true;
             }
-            else if(document.getElementById(`cella-${z}-${j}`).innerText == document.getElementById(`cella-${z + 1}-${j}`).innerText){
-               document.getElementById(`cella-${z + 1}-${j}`).innerText = parseInt(document.getElementById(`cella-${z}-${j}`).innerText)*2;
+            else if (document.getElementById(`cella-${z}-${j}`).innerText == document.getElementById(`cella-${z + 1}-${j}`).innerText) {
+               document.getElementById(`cella-${z + 1}-${j}`).innerText = parseInt(document.getElementById(`cella-${z}-${j}`).innerText) * 2;
                document.getElementById(`cella-${z}-${j}`).innerText = "";
                punti = parseInt(document.getElementById(`cella-${z + 1}-${j}`).innerText);
                document.getElementsByTagName("span")[0].innerText = punti;
+               spostamenti = true;
             }
             z++;
          }
          z = i;
-         while(z < DIM - 1){
-            if(document.getElementById(`cella-${z + 1}-${j}`).innerText == ""){
+         while (z < DIM - 1) {
+            if (document.getElementById(`cella-${z + 1}-${j}`).innerText == "") {
                document.getElementById(`cella-${z + 1}-${j}`).innerText = document.getElementById(`cella-${z}-${j}`).innerText;
                document.getElementById(`cella-${z}-${j}`).innerText = "";
+               spostamenti = true;
             }
-            else if(document.getElementById(`cella-${z}-${j}`).innerText == document.getElementById(`cella-${z + 1}-${j}`).innerText){
-               document.getElementById(`cella-${z + 1}-${j}`).innerText = parseInt(document.getElementById(`cella-${z}-${j}`).innerText)*2;
+            else if (document.getElementById(`cella-${z}-${j}`).innerText == document.getElementById(`cella-${z + 1}-${j}`).innerText) {
+               document.getElementById(`cella-${z + 1}-${j}`).innerText = parseInt(document.getElementById(`cella-${z}-${j}`).innerText) * 2;
                document.getElementById(`cella-${z}-${j}`).innerText = "";
                punti += parseInt(document.getElementById(`cella-${z + 1}-${j}`).innerText);
                document.getElementsByTagName("span")[0].innerText = punti;
+               spostamenti = true;
             }
             z++;
          }
       }
    }
 
-   let pos1 = [0, 0];
-   do{
-      pos1[0] = generaNumero(0, 4);
-      pos1[1] = generaNumero(0, 4);
-   } while (document.getElementById(`cella-${pos1[0]}-${pos1[1]}`).innerText != "");
+   if (!spostamenti) {
+      let lose = checkLose();
+      if (lose) {
+         return;
+      }
+   }
 
-   document.getElementById(`cella-${pos1[0]}-${pos1[1]}`).innerText = "2";
+   generaCella();
 }
 
-function ArrowLeftClicked(){
-   for(let i = DIM - 1; i > -1; i--){
-      for(let j = DIM - 1; j > 0; j--){
+function arrowLeftClicked() {
+   let spostamenti = false;
+   for (let i = DIM - 1; i > -1; i--) {
+      for (let j = DIM - 1; j > 0; j--) {
          let z = DIM - 1;
-         while(z > j){
-            if(document.getElementById(`cella-${i}-${z - 1}`).innerText == ""){
+         while (z > j) {
+            if (document.getElementById(`cella-${i}-${z - 1}`).innerText == "") {
                document.getElementById(`cella-${i}-${z - 1}`).innerText = document.getElementById(`cella-${i}-${z}`).innerText;
                document.getElementById(`cella-${i}-${z}`).innerText = "";
+               spostamenti = true;
             }
-            else if(document.getElementById(`cella-${i}-${z}`).innerText == document.getElementById(`cella-${i}-${z - 1}`).innerText){
-               document.getElementById(`cella-${i}-${z - 1}`).innerText = parseInt(document.getElementById(`cella-${i}-${z}`).innerText)*2;
+            else if (document.getElementById(`cella-${i}-${z}`).innerText == document.getElementById(`cella-${i}-${z - 1}`).innerText) {
+               document.getElementById(`cella-${i}-${z - 1}`).innerText = parseInt(document.getElementById(`cella-${i}-${z}`).innerText) * 2;
                document.getElementById(`cella-${i}-${z}`).innerText = "";
                punti += parseInt(document.getElementById(`cella-${i}-${z - 1}`).innerText);
                document.getElementsByTagName("span")[0].innerText = punti;
+               spostamenti = true;
             }
             z--;
          }
          z = j;
-         while(z > 0){
-            if(document.getElementById(`cella-${i}-${z - 1}`).innerText == ""){
+         while (z > 0) {
+            if (document.getElementById(`cella-${i}-${z - 1}`).innerText == "") {
                document.getElementById(`cella-${i}-${z - 1}`).innerText = document.getElementById(`cella-${i}-${z}`).innerText;
                document.getElementById(`cella-${i}-${z}`).innerText = "";
+               spostamenti = true;
             }
-            else if(document.getElementById(`cella-${i}-${z}`).innerText == document.getElementById(`cella-${i}-${z - 1}`).innerText){
-               document.getElementById(`cella-${i}-${z - 1}`).innerText = parseInt(document.getElementById(`cella-${i}-${z}`).innerText)*2;
+            else if (document.getElementById(`cella-${i}-${z}`).innerText == document.getElementById(`cella-${i}-${z - 1}`).innerText) {
+               document.getElementById(`cella-${i}-${z - 1}`).innerText = parseInt(document.getElementById(`cella-${i}-${z}`).innerText) * 2;
                document.getElementById(`cella-${i}-${z}`).innerText = "";
                punti += parseInt(document.getElementById(`cella-${i}-${z - 1}`).innerText);
                document.getElementsByTagName("span")[0].innerText = punti;
+               spostamenti = true;
             }
             z--;
          }
       }
    }
 
+   if (!spostamenti) {
+      let lose = checkLose();
+      if (lose) {
+         return;
+      }
+   }
+
+   generaCella();
+}
+
+function arrowRightClicked() {
+   let spostamenti = false;
+   for (let i = 0; i < DIM; i++) {
+      for (let j = 0; j < DIM - 1; j++) {
+         let z = 0;
+         while (z < j) {
+            if (document.getElementById(`cella-${i}-${z + 1}`).innerText == "") {
+               document.getElementById(`cella-${i}-${z + 1}`).innerText = document.getElementById(`cella-${i}-${z}`).innerText;
+               document.getElementById(`cella-${i}-${z}`).innerText = "";
+               spostamenti = true;
+            }
+            else if (document.getElementById(`cella-${i}-${z}`).innerText == document.getElementById(`cella-${i}-${z + 1}`).innerText) {
+               document.getElementById(`cella-${i}-${z + 1}`).innerText = parseInt(document.getElementById(`cella-${i}-${z}`).innerText) * 2;
+               document.getElementById(`cella-${i}-${z}`).innerText = "";
+               punti += parseInt(document.getElementById(`cella-${i}-${z + 1}`).innerText);
+               document.getElementsByTagName("span")[0].innerText = punti;
+               spostamenti = true;
+            }
+            z++;
+         }
+         z = j;
+         while (z < DIM - 1) {
+            if (document.getElementById(`cella-${i}-${z + 1}`).innerText == "") {
+               document.getElementById(`cella-${i}-${z + 1}`).innerText = document.getElementById(`cella-${i}-${z}`).innerText;
+               document.getElementById(`cella-${i}-${z}`).innerText = "";
+               spostamenti = true;
+            }
+            else if (document.getElementById(`cella-${i}-${z}`).innerText == document.getElementById(`cella-${i}-${z + 1}`).innerText) {
+               document.getElementById(`cella-${i}-${z + 1}`).innerText = parseInt(document.getElementById(`cella-${i}-${z}`).innerText) * 2;
+               document.getElementById(`cella-${i}-${z}`).innerText = "";
+               punti += parseInt(document.getElementById(`cella-${i}-${z + 1}`).innerText);
+               document.getElementsByTagName("span")[0].innerText = punti;
+               spostamenti = true;
+            }
+            z++;
+         }
+      }
+   }
+
+   if (!spostamenti) {
+      let lose = checkLose();
+      if (lose) {
+         return;
+      }
+   }
+
+   generaCella();
+}
+
+function generaCella() {
    let pos1 = [0, 0];
-   do{
+   do {
       pos1[0] = generaNumero(0, 4);
       pos1[1] = generaNumero(0, 4);
    } while (document.getElementById(`cella-${pos1[0]}-${pos1[1]}`).innerText != "");
@@ -182,45 +257,24 @@ function ArrowLeftClicked(){
    document.getElementById(`cella-${pos1[0]}-${pos1[1]}`).innerText = "2";
 }
 
-function ArrowRightClicked(){
-   for(let i = 0; i < DIM; i++){
-      for(let j = 0; j < DIM - 1; j++){
-         let z = 0;
-         while(z < j){
-            if(document.getElementById(`cella-${i}-${z + 1}`).innerText == ""){
-               document.getElementById(`cella-${i}-${z + 1}`).innerText = document.getElementById(`cella-${i}-${z}`).innerText;
-               document.getElementById(`cella-${i}-${z}`).innerText = "";
-            }
-            else if(document.getElementById(`cella-${i}-${z}`).innerText == document.getElementById(`cella-${i}-${z + 1}`).innerText){
-               document.getElementById(`cella-${i}-${z + 1}`).innerText = parseInt(document.getElementById(`cella-${i}-${z}`).innerText)*2;
-               document.getElementById(`cella-${i}-${z}`).innerText = "";
-               punti += parseInt(document.getElementById(`cella-${i}-${z + 1}`).innerText);
-               document.getElementsByTagName("span")[0].innerText = punti;
-            }
-            z++;
+function checkLose() {
+   let i = 0;
+   let lose = true;
+   while (i < DIM && lose) {
+      let j = 0;
+      while (j < DIM && lose) {
+         if (document.getElementById(`cella-${i}-${j}`).innerText == "") {
+            lose = false;
          }
-         z = j;
-         while(z < DIM - 1){
-            if(document.getElementById(`cella-${i}-${z + 1}`).innerText == ""){
-               document.getElementById(`cella-${i}-${z + 1}`).innerText = document.getElementById(`cella-${i}-${z}`).innerText;
-               document.getElementById(`cella-${i}-${z}`).innerText = "";
-            }
-            else if(document.getElementById(`cella-${i}-${z}`).innerText == document.getElementById(`cella-${i}-${z + 1}`).innerText){
-               document.getElementById(`cella-${i}-${z + 1}`).innerText = parseInt(document.getElementById(`cella-${i}-${z}`).innerText)*2;
-               document.getElementById(`cella-${i}-${z}`).innerText = "";
-               punti += parseInt(document.getElementById(`cella-${i}-${z + 1}`).innerText);
-               document.getElementsByTagName("span")[0].innerText = punti;
-            }
-            z++;
-         }
+         j++
       }
+      i++
    }
 
-   let pos1 = [0, 0];
-   do{
-      pos1[0] = generaNumero(0, 4);
-      pos1[1] = generaNumero(0, 4);
-   } while (document.getElementById(`cella-${pos1[0]}-${pos1[1]}`).innerText != "");
+   if (lose) {
+      alert(`Hai perso!\nIl tuo punteggio è ${punti}`);
+      window.removeEventListener("keydown", keyClicked);
+   }
 
-   document.getElementById(`cella-${pos1[0]}-${pos1[1]}`).innerText = "2";
+   return lose;
 }
